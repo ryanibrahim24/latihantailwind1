8a9a4678325a4aa881e1c1def8749f8314a4d2ca
ryanibrahim24 <ryanibrahim27@gmail.com>
2023-05-22 20:22:58 +0700

    responsive

---
+++ public/logic.js
@@ -0,0 +1,16 @@
+const cobain = document.querySelectorAll('.card');
+function klik(target) {
+  if(target.classList == 'bg-white') {
+    
+  }
+}
+for (const i of cobain) {
+  i.addEventListener('click', klik(i))
+}
+
+
+
+
+
+
+
